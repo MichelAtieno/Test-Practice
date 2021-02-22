20ab0afe50b6a2f4f9c0bce28b943eb7d13fc5f8
Michel Atieno <mishqamish@gmail.com>
2021-02-22 13:44:21 +0300

    Created CSV file

--- twitter.py
+++ twitter.py
@@ -1,4 +1,4 @@
-import tweepy
+import tweepy, csv
 
 consumer_key = "Y0HnPFqOLaBt9H6rGCq5e6hd5"
 consumer_secret = "dwqrD2j7rDhcAuUfC4AR9WvbDLE1jhp0DOdXrSrDg3oeqxgDie"
@@ -13,6 +13,25 @@ api = tweepy.API(auth)
 api.verify_credentials()
 
 ##Search for Acute Encephalitis Syndrome
-search = api.search(q="Acute Encephalitis Syndrome", lang="en", rpp=100, count=100)
+# search = api.search(q="Acute Encephalitis Syndrome", lang="en", rpp=100, count=100)
 
-print(search)
+# for tweet in search:
+#     print(tweet.text, tweet.created_at, tweet.user.location )
+
+# print(len(search))
+
+## Task: Is to get at least 1000 tweets from the twitter API and save it
+## To a CSV file
+
+file_out = open("tweets.csv", "w+")
+writer = csv.writer(file_out)
+
+i=0
+for item in tweepy.Cursor(api.search, q="Encephalitis Syndrome").items(100):
+    tweet = []
+    print("Adding item",i, item.id)
+    i+=1
+    tweet.append(item.created_at)
+    tweet.append(item.user.screen_name)
+    tweet.append(item.text)
+    writer.writerow(tweet)
